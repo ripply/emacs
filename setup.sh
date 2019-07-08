0f7485c3ed3257043bbadd14b3dc06fb28b3e0f8
Chase Ripplinger <chase@ripply.io>
2019-07-08 12:40:06 -0500

    workaround for projectile

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 ln -s `pwd`/.spacemacs ~/
 ln -s `pwd`/.xmodmap ~/
+# https://github.com/syl20bnr/spacemacs/issues/9374#issuecomment-482924249
+find ~/.emacs.d/elpa -name "*elc" -delete
